cf31b04a90a62db199f89f4c68125d08f2b75318
AudunKodehode <audunkodehode@gmail.com>
2023-11-03 20:21:32 +0100

    *

--- javascript.js
+++ javascript.js
@@ -1,4 +1,22 @@
 const ipaddressInput = document.getElementById("ipaddressInput");
-ipaddressInput.addEventListener("onChange", (event) => {
-    console.log(event.target.value);
-})
+let ipAddress = "10.0.0.17";
+function command(command){
+    if (command == "cd"){
+
+        fetch("10.0.0.17/command?command=cd", {
+            method: "POST",
+            body: JSON.stringify(data)
+          });
+    }
+}
+let data = {command: "cd"};
+
+function setIP(){
+    if (ipaddressInput.value != ""){
+        ipAddress = ipaddressInput.value;
+        console.log("IP set to: " + ipAddress);
+    }
+}
+
+
+
