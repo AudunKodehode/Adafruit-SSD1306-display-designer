12cb2356581f497beb4270257b24f5a462a08a14
AudunKodehode <audunkodehode@gmail.com>
2023-11-03 20:52:24 +0100

    *

--- javascript.js
+++ javascript.js
@@ -1,19 +1,28 @@
 const ipaddressInput = document.getElementById("ipaddressInput");
 let ipAddress = "10.0.0.17";
-function command(command){
-    if (command == "cd"){
-
-        fetch("10.0.0.17/command?command=cd", {
-            method: "POST",
-            body: JSON.stringify(data)
-          });
+function command(command) {
+    if (command === "cd") {
+      fetch("http://"+ipAddress, {
+        method: "POST",
+        body: JSON.stringify(data),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          // Handle a successful response here
+        })
+        .catch((error) => {
+          console.error("Fetch error:", error);
+        });
     }
-}
+  }
 let data = {command: "cd"};
 
 function setIP(){
     if (ipaddressInput.value != ""){
         ipAddress = ipaddressInput.value;
+        document.getElementById("ipaddressSpan").textContent = "Set IP: " + ipAddress;
         console.log("IP set to: " + ipAddress);
     }
 }
