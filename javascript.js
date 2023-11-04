0d258a34605613bee8c7d5636b84d071760ee865
AudunKodehode <audunkodehode@gmail.com>
2023-11-04 17:02:12 +0100

    *

--- javascript.js
+++ javascript.js
@@ -18,6 +18,7 @@ let y2 = "0";
 let width = "0";
 let height = "0";
 let drawOrFill = "fill";
+let commandNumber = 0;
 
 const customCommandInput = document.getElementById("customCommandInput");
 const customCommandSendButton = document.getElementById(
@@ -42,6 +43,8 @@ const y2Input = document.getElementById("y2Input");
 const widthInput = document.getElementById("widthInput");
 const heightInput = document.getElementById("heightInput");
 const textInput = document.getElementById("textInput");
+const ssd1306Code = document.getElementById("ssd1306Code");
+const customCode = document.getElementById("customCode");
 function screenWidthChange() {
   screenWidth = screenWidthInput.value.toString();
   x0Input.max = screenWidthInput.value - 1;
@@ -98,14 +101,12 @@ function radio(type) {
 function radiusChange() {
   radius = radiusInput.value.toString();
 }
-function widthChange(){
+function widthChange() {
   width = widthInput.value.toString();
 }
-function heightChange(){
-    height = heightInput.value.toString();
-  }
-
-
+function heightChange() {
+  height = heightInput.value.toString();
+}
 
 function textSizeSliderChange() {
   textSizeSpan.textContent = "TextSize: " + textSizeSlider.value;
@@ -134,6 +135,7 @@ let lastCommand = "";
 customCommandInput.addEventListener("keyup", function (event) {
   if (event.key === "Enter") {
     customCommandSendButton.click();
+
     lastCommand = customCommandInput.value;
     customCommandInput.value = "";
   }
@@ -142,16 +144,49 @@ customCommandInput.addEventListener("keyup", function (event) {
   }
 });
 
-function command(command) {
-    if (command == "pixel") {
-        command = "dp" + x0 + "," + y0 + "," + color ;
-      }
-      if (command == "line") {
-        command = "dl" + x0 + "," + y0 + "," + x1 + "," + y1 + "," + color ;
-      }
-if (command == "invert"){
-    command = "id";
+function addCustom(command) {
+  const newCommand = document.createElement("p");
+  customCode.appendChild(newCommand);
+  newCommand.id = commandNumber;
+  const deleteButton = document.createElement("button");
+  deleteButton.style.width = "20px";
+  deleteButton.style.height = "20px";
+  deleteButton.style.marginRight = "5px";
+  deleteButton.style.backgroundColor = "gray";
+  deleteButton.style.borderRadius = "5px";
+  deleteButton.addEventListener("mouseover", function () {
+    deleteButton.style.backgroundColor = "red";
+  });
+  deleteButton.addEventListener("mouseout", function () {
+    deleteButton.style.backgroundColor = "gray";
+  });
+
+  deleteButton.className = "deleteButton";
+  deleteButton.onclick = function () {
+    customCode.removeChild(newCommand);
+  };
+  newCommand.appendChild(deleteButton);
+  let commandText = document.createTextNode(command);
+  newCommand.appendChild(commandText);
 }
+
+function command(command) {
+  if (command == "cd") {
+    command = "cd";
+    addCustom(command);
+  }
+  if (command == "pixel") {
+    command = "dp" + x0 + "," + y0 + "," + color;
+    addCustom(command);
+  }
+  if (command == "line") {
+    command = "dl" + x0 + "," + y0 + "," + x1 + "," + y1 + "," + color;
+    addCustom(command);
+  }
+  if (command == "invert") {
+    command = "id";
+    addCustom(command);
+  }
 
   if (command == "circle") {
     if (drawOrFill == "fill") {
@@ -159,7 +194,8 @@ if (command == "invert"){
     } else {
       command = "dc";
     }
-    command += x0 + "," + y0 + "," + radius + "," + color ;
+    command += x0 + "," + y0 + "," + radius + "," + color;
+    addCustom(command);
   }
   if (command == "rectangle") {
     if (drawOrFill == "fill") {
@@ -167,7 +203,9 @@ if (command == "invert"){
     } else {
       command = "dr";
     }
-    command += x0 + "," + y0 + "," + width + "," + height + "," + radius + "," + color;
+    command +=
+      x0 + "," + y0 + "," + width + "," + height + "," + radius + "," + color;
+    addCustom(command);
   }
   if (command == "triangle") {
     if (drawOrFill == "fill") {
@@ -175,21 +213,28 @@ if (command == "invert"){
     } else {
       command = "dt";
     }
-    command += x0 + "," + y0 + "," + x1 + "," + y1 + "," + x2 + "," + y2 + "," + color;
+    command +=
+      x0 + "," + y0 + "," + x1 + "," + y1 + "," + x2 + "," + y2 + "," + color;
+    addCustom(command);
   }
-if (command == 'newLine'){
-    command = 'nl';
-}
-if (command == 'newtext'){
-    command = 'nt' + x0 + "," + y0 + "," + color + "," + textSize +  textInput.value
-}
-if (command == 'printText'){
-    command = 'pt' + x0 + "," + y0 + "," + color + "," + textSize +  textInput.value
-}
-if (command == 'continueText'){
-    command = 'ct' + textInput.value
-}
-
+  if (command == "newLine") {
+    command = "nl";
+    addCustom(command);
+  }
+  if (command == "newtext") {
+    command =
+      "nt" + x0 + "," + y0 + "," + color + "," + textSize + textInput.value;
+    addCustom(command);
+  }
+  if (command == "printText") {
+    command =
+      "pt" + x0 + "," + y0 + "," + color + "," + textSize + textInput.value;
+    addCustom(command);
+  }
+  if (command == "continueText") {
+    command = "ct" + textInput.value;
+    addCustom(command);
+  }
 
   const url = `http://${ipAddress}/command?command=${command}`;
   fetch(url, requestOptions)
@@ -202,6 +247,10 @@ if (command == 'continueText'){
 function customCommand(command) {
   if ((command = "customCommand")) {
     command = customCommandInput.value;
+    if (!command.includes(";")) {
+      command += ";";
+    }
+    addCustom(command);
   }
   const url = `http://${ipAddress}/command?command=${command}`;
   fetch(url, requestOptions)
@@ -217,4 +266,36 @@ function setIP() {
     ipAddress = ipaddressInput.value;
     ipaddressSpan.textContent = "Set IP: " + ipAddress;
   }
+}
+function clearAll() {
+  customCode.textContent = "";
+  ssd1306Code.textContent = "";
+}
+function clearCustom() {
+  customCode.textContent = "";
+}
+
+function clearSSD() {
+  ssd1306Code.textContent = "";
+}
+
+function copySSD() {
+  var copyText = document.getElementsByClassName("ssd1306Code")[0];
+  var range = document.createRange();
+  range.selectNode(copyText);
+  var selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand("copy");
+  selection.removeAllRanges();
+}
+function copyCustom() {
+  var copyText = document.getElementsByClassName("customCode")[0];
+  var range = document.createRange();
+  range.selectNode(copyText);
+  var selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  document.execCommand("copy");
+  selection.removeAllRanges();
 }
